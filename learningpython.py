e63f31956d56a289a0c566433aedf16d4edc8047
aprajita mrinal <aparajitamrinnal@gmail.com>
2026-06-28 20:22:15 +0530

    updated

--- learningpython.py
+++ learningpython.py
@@ -1,1 +1,46 @@
-print("helloww!")
+#print("helloww!")
+#strings
+'''
+mystring = "hello"
+print(mystring)
+'''
+#for new line we musst use \n
+'''
+str1 = "what are you doing,\nbruh"
+
+print(str1)
+'''
+
+#same for tab we can use \t
+'''
+str2 = "what are you doing,\tbruh"
+print(str2)
+'''
+
+#basic operations (1.concatination)
+'''
+str1 = "hello"
+str2 = "world"
+str3 = str1 + str2
+print(str3)
+'''
+# (2. length of str)
+'''
+str1 = "hello"
+print(len(str1))
+
+str2 = "world"
+len1 = len(str2)
+print(len1)
+'''
+#(3.INDEXING)
+'''
+s1 = 'hi lol'
+print(s1[0])
+print(s1[1])
+print(s1[2])
+'''
+#(4. slicing)
+
+
+
